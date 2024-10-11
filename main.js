1db56339dbd2cbc7d900d1c617343279f5f56a5f
𝓕𝓽𝓶 <s.sh.d5232@gmail.com>
2024-10-11 16:39:15 +0330

    card

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const burgerMenu = document.querySelector(".burger-menu__list")
+const burgerMenu = document.querySelector(".burger-menu__list");
 const togglers = document.querySelector(".burger-menu__toggle");
 const logoIcon = document.querySelector(".logo__icon");
 
 console.log(togglers);
 function toggleBurgerMenu () {
-    burgerMenu.classList.toggle("burger-menu--collapse")
-    
+    burgerMenu.classList.toggle("burger-menu--collapse");
 }
 togglers.addEventListener("click", toggleBurgerMenu);
 
