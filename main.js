aae8f165354a4357431d3990a054cf63ff878a3e
𝓕𝓽𝓶 <s.sh.d5232@gmail.com>
2024-10-11 16:25:14 +0330

    final

---
+++ main.js
@@ -0,0 +1,12 @@
+const burgerMenu = document.querySelector(".burger-menu__list")
+const togglers = document.querySelector(".burger-menu__toggle");
+const logoIcon = document.querySelector(".logo__icon");
+
+console.log(togglers);
+function toggleBurgerMenu () {
+    burgerMenu.classList.toggle("burger-menu--collapse")
+    
+}
+togglers.addEventListener("click", toggleBurgerMenu);
+
+
